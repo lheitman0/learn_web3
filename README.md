adb1ee2ce9e1c88d83e605067f27bf0fb3a49d1b
Luke Heitman <lh5938a@student.american.edu>
2023-11-01 20:28:36 -0400

    commit

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ for me to put practice projects revolving around web3 and blockchain
 
 token design -- Stone Token, STN
 
-1) initial supply is sent to owner --> 50% of max supply
-2) capped supply --> 100 m?
-3) remaining supply is used for minting system (mining reward?)
+1) initial supply is sent to owner --> 50% of max supply [done]
+2) capped supply --> 100 m? [done]
+3) remaining supply is used for minting system (mining reward?)[done]
+4) create block reward
+    _beforeTokenTransfer --> control what happens right before transfer complete
+    _mintMinerReward --> control what the mint reward is for each block mined
--- stone-token/contracts/StoneToken.sol
+++ stone-token/contracts/StoneToken.sol
@@ -3,10 +3,35 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract myToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("myToken","MT"){
-        _mint(msg.sender, initialSupply);
+
+
+contract myToken is ERC20, ERC20Burnable {
+    // declare owner var
+    address payable public owner;
+    uint256 public blockReward;
+
+    constructor(uint256 cap, uint256 reward) ERC20("StoneToken","STN") ERC20Capped(cap * (10 ** decimals())){
+        owner = msg.sender;
+        // decimals is hardcoded in as uint 18
+        _mint(owner, 50 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals());
     }
-  
+    // interal function means can only be called within the contract
+    function mintMinerReward() interal{
+        // _mint just declares that we're calling from an inherited contract
+        _mint(block.coinbase, blockReward);
+    }
+
+    function setBlockReward(uint256 reward) public onlyOwner{
+        blockReward = reward * (10**decimals());
+    }
+    // access modifier to implement in block reward so only they can edit
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "only owner can call this function");
+        _;
+    }
 }
